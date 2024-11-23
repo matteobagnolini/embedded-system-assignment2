5cfe1a00eea7c11954b615869655f69b8faeb7ed
Matteo Bagnolini <matteobagnolini2003@gmail.com>
2024-11-23 12:30:50 +0100

    add defines in main file

--- arduino/src/WasteDisposal.cpp
+++ arduino/src/WasteDisposal.cpp
@@ -4,6 +4,12 @@
 #include "tasks/wasteDetectTask.h"
 
 #define BASE_PERIOD 50
+#define TEMP_TASK_PERIOD 150
+#define WASTE_DET_TASK_PERIOD 150
+
+#define TEMP_PIN A0
+#define WASTE_DET_TRIGGPIN 7
+#define WASTE_DET_ECHOPIN 8
 
 Scheduler sched;
 
@@ -11,12 +17,12 @@ void setup() {
     Serial.begin(9600);
     sched.init(BASE_PERIOD);
 
-    Task *t0 = new TempDetectTask(A0);
-    t0->init(150);
+    Task *t0 = new TempDetectTask(TEMP_PIN);
+    t0->init(TEMP_TASK_PERIOD);
     sched.addTask(t0);
 
-    Task *t1 = new WasteDetectTask(7, 8);
-    t1->init(150);
+    Task *t1 = new WasteDetectTask(WASTE_DET_TRIGGPIN, WASTE_DET_ECHOPIN);
+    t1->init(WASTE_DET_TASK_PERIOD);
     sched.addTask(t1);
 }
 
